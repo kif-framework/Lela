a38c7ab34b15e8c0cdf2a6315f6b4ac156331ec7
Brian Nickel <brian.nickel@gmail.com>
2013-07-12 22:58:19 -0700

    Initial commit.

---
+++ Lela/Lela.h
@@ -0,0 +1,13 @@
+//
+//  Lela.h
+//  Lela
+//
+//  Created by Brian Nickel on 7/12/13.
+//  Copyright (c) 2013 Brian Nickel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lela : NSObject
+
+@end
